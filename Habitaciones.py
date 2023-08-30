aafe264dfecc574f102825f3a54491313b871d03
Mauricio Polo <mauriciopoloem@gmail.com>
2023-08-29 23:51:44 -0500

    Hotel POO

---
+++ Habitaciones.py
@@ -0,0 +1,83 @@
+
+class Rooms:
+
+    def __init__(self, id, type, price, available=True):
+        self.id=id
+        self.type=type
+        self.price=price
+        self.available=available
+        self.resident=None
+    
+    def inf(self):
+        pass
+
+    def reserve(self):
+        self.available=False
+
+    def cost(self, nights):
+        pass
+    
+    def guest(self, resident):
+        self.resident=resident
+        
+    def Benefits(self):
+        print("Aqui van los beneficios")
+
+class vip(Rooms):
+    
+    def __init__(self, id, available=True):
+        super().__init__(id, "VIP", 120000, available)
+        
+    def inf(self):
+        print("-Espaciosa \n-Cama King size \n-Aire acondicionado \n-TV 62 pulagada \n-Desayuno a la habitacion \nJacuzzi")
+
+    def Benefits(self):
+        return print("-20% de descuento si se queda mas de 3 dias \n-Desayuno a la habitacion \n-Postre gratis en el restaurante \n-Botella gratis Blue Label en el bar de 750ml")
+    
+    def cost(self, nights):
+        if nights > 3:
+            return (self.price*nights)*0.8 
+        else:
+            return self.price*nights
+
+
+class simple(Rooms):
+    
+    def __init__(self, id, available=True):
+        super().__init__(id, "Sencilla", 50000, available)
+        
+    def inf(self):
+        print("-Espacio 4X3 \n-Cama de un cuerpo \n-Ventilador \n-TV 32 pulgadas")
+
+    def Benefits(self):
+        print("-5% de descuento si se queda mas de 3 dia ")
+    
+    def cost(self, nights):
+        if nights > 3:
+            return (self.price*nights)*0.95 
+        else:
+            return self.price*nights
+
+
+class double(Rooms):
+    
+    def __init__(self, id, available=True):
+        super().__init__(id, "Doble", 80000, available)
+        
+    def inf(self):
+        print("-Espaciosa \n-Cama de dos cuerpos \n-Ventilador \n-TV 52 pulgadas")
+
+    def Benefits(self):
+        print("-10% de descuento si se queda mas de 3 dias \n-Desayuno gratis")
+
+    def cost(self, nights):
+        if nights > 3:
+            return (self.price*nights)*0.9 
+        else:
+            return self.price*nights
+    
+
+
+
+na=vip(123, True)
+print(na.cost(5))
---
+++ menu.py
@@ -0,0 +1,56 @@
+from Habitaciones import *
+from Hotel import TreeHotel
+
+Tree=TreeHotel()
+Tree.add(simple(101, True))
+Tree.add(double(111, True))
+Tree.add(vip(112, False))
+Tree.add(simple(113, True))
+Tree.add(double(114, True))
+Tree.add(double(102, True))
+Tree.add(vip(103, True))
+Tree.add(vip(115, False))
+Tree.add(simple(104, True))
+Tree.add(double(105, True))
+Tree.add(vip(106, True))
+Tree.add(simple(107, True))
+Tree.add(double(108, False))
+Tree.add(vip(109, True))
+Tree.add(simple(110, True))
+Tree.add(double(105, True))
+Tree.add(vip(106, True))
+
+
+
+menu= '''
+Bienvenido al hotel Miami
+Elije una opcion
+1.Ver todas las habitaciones del hotel
+2.Reservar una habitacion
+3.Calcular precio
+4.Ver informacion de la habitacion
+5.Ver Beneficios de la habitacion
+6.Salir
+'''  
+
+op = 0
+while op != 6 and op>=0 and op <=5:
+    op=int(input(menu))
+    if op==1:
+        Tree.print_inorder(Tree.root)
+    elif op==2:
+        name1=input("Ingrese su nombre ")
+        cc=int(input("Ingrese su cedula "))
+        typeroom=input("¿Que tipo de habitacion desea? ")
+        Tree.search_available(Tree.root, typeroom, name1, cc)
+    elif op==3:
+        typeh1=input("Que tipo de habitacion desea? ")
+        nights=int(input("Cuantos dias desea quedarse? "))
+        print(f"Recuerde que si se queda mas de 3 dias tendra un descuento \nEl costo total de {nights} dias es de {Tree.cost1(typeh1, nights)}")
+    elif op==4:
+        typeh2=input("De que tipo de habitacio desea ver informacion? ")
+        Tree.inf_room(typeh2)
+    elif op==5:
+        typeh3=input("De que habitacion desea ver los beneficios? ")
+        Tree.inf_room(typeh3)
+        
