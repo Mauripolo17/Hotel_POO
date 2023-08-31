2ba0214b05687e8bf470f16e82e5bc512f02759a
Mauripolo17 <mauripolo14@gmail.com>
2023-08-31 00:22:27 -0500

    hhh

--- Huesped.py
+++ Huesped.py
@@ -5,7 +5,6 @@ class guest:
         self.cc=cc
         self.tg=tg
 
-
     def str(self):
         return print(f"Nombre: {self.name} CC {self.cc} tipo {self.tg}")
     
@@ -20,7 +19,6 @@ class premium_guest(guest):
         print("-20% de descuento si se queda mas de 3 dias \n-Desayuno a la habitacion \n-Postre gratis en el restaurante \n-Botella gratis Blue Label en el bar de 750ml")
 
         
-
 class simple_guest(guest):
     def __init__(self, name, cc):
         super().__init__(name, cc, "VIP")
